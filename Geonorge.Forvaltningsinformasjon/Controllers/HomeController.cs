1e3a4fc585605f2ff113dd09d8def561b28f9b09
Dag Olav Dahle <dagolav@arkitektum.no>
2018-01-31 14:49:42 +0100

    Use asp.net core 2

--- Geonorge.Forvaltningsinformasjon/Controllers/HomeController.cs
+++ Geonorge.Forvaltningsinformasjon/Controllers/HomeController.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Geonorge.Forvaltningsinformasjon.Models;
 
 namespace Geonorge.Forvaltningsinformasjon.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- Geonorge.Forvaltningsinformasjon/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Geonorge.Forvaltningsinformasjon.MvcApplication" Language="C#" %>

c2224c72c2b19518a26b2c69c78c773b52e54040
Henning Jensen <henning@arkitektum.no>
2018-09-05 08:25:23 +0200

    Upgrade project to dot net core 2.1

--- Geonorge.Forvaltningsinformasjon/Startup.cs
+++ Geonorge.Forvaltningsinformasjon/Startup.cs
@@ -29,7 +29,6 @@ namespace Geonorge.Forvaltningsinformasjon
         {
             if (env.IsDevelopment())
             {
-                app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
             }
             else

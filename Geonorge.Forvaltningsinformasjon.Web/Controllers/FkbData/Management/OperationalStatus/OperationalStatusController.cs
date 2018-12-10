da9865bc5c9057189d21e6f5e45c53886d4e257e
Attila Torjak <attila.torjak@kartverket.no>
2018-12-10 13:19:46 +0100

    Operational state without styling. Inc 2.

--- Geonorge.Forvaltningsinformasjon.Web/Controllers/FkbData/Management/OperationalStatus/OperationalStatusController.cs
+++ Geonorge.Forvaltningsinformasjon.Web/Controllers/FkbData/Management/OperationalStatus/OperationalStatusController.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.ServiceModel.Syndication;
 using System.Threading.Tasks;
+using System.Xml;
 using System.Xml.Linq;
 using Geonorge.Forvaltningsinformasjon.Models;
 using Geonorge.Forvaltningsinformasjon.Web.Abstractions.FkbData.Management.Helpers;
 using Geonorge.Forvaltningsinformasjon.Web.Models.FkbData.Management;
+using Geonorge.Forvaltningsinformasjon.Web.Models.FkbData.Management.OperationalStatus;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace Geonorge.Forvaltningsinformasjon.Web.Controllers.FkbData.Management.OperationalStatus
 {
@@ -27,14 +31,24 @@ namespace Geonorge.Forvaltningsinformasjon.Web.Controllers.FkbData.Management.Op
         [HttpGet("")]
         public IActionResult Index([FromQuery] int id, [FromQuery]bool isCounty)
         {
-            WebClient wclient = new WebClient();
-            string RSSData = wclient.DownloadString(_operationalStatus);
+            XmlTextReader reader = new XmlTextReader(_operationalStatus);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            List<AtomFeedEntryViewModel> entries = new List<AtomFeedEntryViewModel>();
 
-            XDocument xml = XDocument.Parse(RSSData);
+            foreach (SyndicationItem item in feed.Items ?? Enumerable.Empty<SyndicationItem>())
+            {
+                entries.Add(new AtomFeedEntryViewModel()
+                {
+                    Title = item.Title.Text,
+                    Content = ((TextSyndicationContent)item.Content).Text,
+                    Published = item.PublishDate.DateTime,
+                    Updated = item.LastUpdatedTime.DateTime
+                });
+            }
 
             ViewBag.ContextViewModel = _contextViewModelHelper.Create(_contextViewModelHelper.Id2Key(id, isCounty));
             ViewBag.ContextViewModel.Aspect = ContextViewModel.EnumAspect.OperationalStatus;
-            return View("Views/FkbData/Management/OperationalStatus/OperationalStatus.cshtml");
+            return View("Views/FkbData/Management/OperationalStatus/OperationalStatus.cshtml", entries);
         }
     }
 }

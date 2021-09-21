8f2c190c359f2a0ecee51e3785752b6504ce742b
Dag Olav Dahle <dagolav@arkitektum.no>
2021-09-21 12:41:53 +0200

    Project state tooltip

--- Geonorge.Forvaltningsinformasjon.Web/Models/MappingProjects/Geovekst/MappingProjectViewModel.cs
+++ Geonorge.Forvaltningsinformasjon.Web/Models/MappingProjects/Geovekst/MappingProjectViewModel.cs
@@ -118,6 +118,7 @@ namespace Geonorge.Forvaltningsinformasjon.Web.Models.MappingProjects.Geovekst
         public string MunicipalityToolTip { get; }
         public string DeliveryTypes { get; }
         public string State { get; }
+        public string StateToolTip { get; }
 
         public List<IMappingProjectDelivery> Deliveries { get; }
 
@@ -171,6 +172,29 @@ namespace Geonorge.Forvaltningsinformasjon.Web.Models.MappingProjects.Geovekst
 
             // state
             State = GetStateName(project.State);
+            StateToolTip = GetStateToolTip(project.State);
+        }
+
+        private static string GetStateToolTip(MappingProjectState state)
+        {
+            string name;
+
+            switch (state)
+            {
+                case MappingProjectState.Delivered:
+                    name = "Alle data er levert og tilgjengeliggjort, men prosjektet er ikke formelt avsluttet.";
+                    break;
+                case MappingProjectState.Ongoing:
+                    name = "Prosjektet er pågående.";
+                    break;
+                case MappingProjectState.Closed:
+                    name = "Prosjektet er avsluttet.";
+                    break;
+                default:
+                    name = "";
+                    break;
+            }
+            return name;
         }
     }
 }

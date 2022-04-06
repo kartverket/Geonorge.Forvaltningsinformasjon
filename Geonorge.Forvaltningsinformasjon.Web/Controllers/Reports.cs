ca8b87246f6c155b99fc256346ac48ca97c7903a
Dag Olav Dahle <dagolav@arkitektum.no>
2022-04-06 13:54:29 +0200

    Test iframe

---
+++ Geonorge.Forvaltningsinformasjon.Web/Controllers/Reports.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Geonorge.Forvaltningsinformasjon.Web.Controllers
+{
+    public class Reports : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

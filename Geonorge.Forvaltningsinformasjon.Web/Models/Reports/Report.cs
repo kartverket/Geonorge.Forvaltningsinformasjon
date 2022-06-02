74a394b5878a8cbef65c76040dca6f32d5a8e0c1
Dag Olav Dahle <dagolav@arkitektum.no>
2022-06-02 14:30:31 +0200

    Display html report

---
+++ Geonorge.Forvaltningsinformasjon.Web/Models/Reports/Report.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Geonorge.Forvaltningsinformasjon.Web.Models.Reports
+{
+    public class Report
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Beskrivelse { get; set; }
+
+        public string FylkesNummer { get; set; }
+        public string FylkesNavn { get; set; }
+        public DateTime? Tidspunkt { get; set; }
+        public string HTMLRapport { get; set; }
+        public string KommuneNummer { get; internal set; }
+        public string KommuneNavn { get; internal set; }
+    }
+}
